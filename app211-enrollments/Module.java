fc154c5add8c340319b38e11a65d1d1521506fee
Derek Peacock <derek.peacock@yahoo.com>
2021-10-08 12:05:53 +0100

    Create Module.java

---
+++ app211-enrollments/Module.java
@@ -0,0 +1,25 @@
+
+/**
+ * This class will maintan information on
+ * course modules
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Module
+{
+    public static final int credit = 15;
+    private String code;
+    private String title;
+    
+
+    /**
+     * Constructor for objects of class Module
+     */
+    public Module()
+    {
+        // initialise instance variables
+    }
+
+  
+}
